e33aeae5f608a574155370c6bd905b5210175548
Gustavo Oliveira <gustavo.h.oliveira7531@gmail.com>
2024-03-01 20:17:23 -0300

    first commit

---
+++ championship/src/test/java/championship/championship/ChampionshipApplicationTests.java
@@ -0,0 +1,13 @@
+package championship.championship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
